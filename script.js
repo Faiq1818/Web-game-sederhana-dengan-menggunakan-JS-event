b8bf75712d6e48cb27604809d502b8b7bbeac7fa
Faiq Ghozy Erlangga <155573422+Faiq1818@users.noreply.github.com>
2024-06-10 15:13:59 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 let gambar1 = document.getElementById("gambar1");
 let gambar2 = document.getElementById("gambar2");
+let like = document.getElementById("like");
+let dislike = document.getElementById("dislike");
 
+
+function setwaktu(kondisi){
+    kondisi.style.display = "block";
+    setTimeout(function() {
+        kondisi.style.display = "none";
+    }, 1000);
+}
+
+
+gambar1.addEventListener('click', function() {
+    setwaktu(like)
+});
+gambar2.addEventListener('click', function() {
+     setwaktu(dislike)
+});
